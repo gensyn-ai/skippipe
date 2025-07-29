3ae9ef40dce1e33342bb23d8cbe36902ee784d09
NikolayBlagoev <workerthreadllc@gmail.com>
2025-07-29 17:31:50 +0300

    fix

--- simulate-training/trainer.py
+++ simulate-training/trainer.py
@@ -35,7 +35,7 @@ tokenizer = SPTokenizer()
 padding_idx = tokenizer.eos_id
 train_ds = RedPyjamav2(tokenizer, batch_size=8, skip = 100, seq_l=ctx_size, name="sample-1T")
 val_ds = RedPyjamav2(tokenizer, batch_size=8, skip = 0, seq_l=ctx_size, name="sample-1T")
-net = LLama(SwapLLama,tokenizer.vocab_size, dmodel=dim, num_heads=kv_heads, n_layers=layers, ctx_size=ctx_size, padding_idx=padding_idx, device=device)
+net = LLama(SwapLLama,tokenizer.vocab_size, dmodel=dim, num_heads=kv_heads, n_layers=layers, ctx_size=ctx_size, device=device)
 with open("2_communication_8_samples_llama_500M.json","r") as fd:
     config = json.load(fd)
 paths = config["ca-paths"]
